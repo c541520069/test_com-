193d2e0474c44d16d068d1c78e9550fce139013f
codebuddy <codebuddy@cym.com>
2026-05-14 15:51:04 +0800

    idea

---
+++ PrimeNumbers.java
@@ -0,0 +1,25 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("100以内的质数：");
+
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
